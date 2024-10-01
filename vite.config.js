75a24dce77da54a63b2c95b127cd84c1b3bbee06
krishnadesai66 <krishnasdesai6@gmail.com>
2024-09-30 20:56:59 -0400

    altered vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    base: 'https://krishnadesai66.github.io/',
+    open: true, 
+  },
+  build: {
+    outDir: 'dist', 
+  },
+});
