b7615cc2357e83cd8ebb2d3801c38cf221ea8204
krishnadesai66 <35243884+krishnadesai66@users.noreply.github.com>
2024-09-30 20:03:43 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Header';
-import Home from './Home';
+import Home from './home';
 import About from './About';  
 import Projects from './Projects';  
 
