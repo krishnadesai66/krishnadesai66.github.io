ed1b90621370527540ea821554be6530b932506d
krishnadesai66 <krishnasdesai6@gmail.com>
2024-09-30 21:09:34 -0400

    changes to deploy source

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    base: 'https://krishnadesai66.github.io/',
+    base: 'https://krishnadesai66.github.io',
     open: true, 
   },
   build: {
